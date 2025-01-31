2e488db21f757edf96979d583507be4766d7b6c8
Daksh Hanswal <dakshhanswal@gmail.com>
2025-01-31 11:39:41 +0530

    Create 2x1_mux.v

---
+++ multiplexer/2x1_mux.v
@@ -0,0 +1,35 @@
+//design
+
+module mux (output out,
+                input a, b, s);
+  
+  assign out = s ? b:a;
+  
+endmodule
+
+//testbench
+
+module stimulus;
+  reg A, B, S;
+  wire OUT;
+  integer i;
+  
+  mux i1 (.a(A), .b(B), .s(S), .out(OUT));
+  
+  initial
+    begin
+      
+      A <= 1;
+      B <= 0;
+      S <= 0;
+      
+      $monitor ("a=%b, b=%b, s=%b, out=%b", A,B,S,OUT);
+      
+      for (i = 0; i <2; i=i+1)
+        begin
+          S <= i;
+          #10;
+        end
+      
+    end
+endmodule
