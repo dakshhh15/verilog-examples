909c688d01f26c40fbde6dd15c9e022dd7a37433
Daksh Hanswal <dakshhanswal@gmail.com>
2025-02-10 18:22:44 +0530

    Update control_unit_top.v

--- single cycle risc v processor/control_unit_top.v
+++ single cycle risc v processor/control_unit_top.v
@@ -1,31 +1,24 @@
-module Control_Unit_Top (Op,RegWrite,ImmSrc,ALUSrc,MemWrite,ResultSrc,Branch,funct3,funct7,ALUControl);
-
-    input [6:0]Op,funct7;
-    input [2:0]funct3;
-    output RegWrite,ALUSrc,MemWrite,ResultSrc,Branch;
-    output [1:0]ImmSrc;
-    output [2:0]ALUControl;
-
-    wire [1:0]ALUOp;
-
-    Main_Decoder Main_Decoder(
-                .Op(Op),
-                .RegWrite(RegWrite),
-                .ImmSrc(ImmSrc),
-                .MemWrite(MemWrite),
-                .ResultSrc(ResultSrc),
-                .Branch(Branch),
-                .ALUSrc(ALUSrc),
-                .ALUOp(ALUOp)
-    );
-
-    ALU_Decoder ALU_Decoder(
-                            .ALUOp(ALUOp),
-                            .funct3(funct3),
-                            .funct7(funct7),
-                            .op(Op),
-                            .ALUControl(ALUControl)
-    );
-
-
+module Control_Unit_Top (output [2:0] ALUControl,
+                         output [1:0] ImmSrc,
+                         output ResultSrc, MemWrite, ALUSrc, RegWrite, Branch,
+                         input [6:0] Op, funct7,
+                         input [2:0] funct3);
+  
+  wire [1:0] ALU;
+  
+  ALU_Decoder ALU_Decoder (.ALUControl(ALUControl),
+                           .ALUOp(ALU),
+                           .op(Op),
+                           .funct7(funct7),
+                           .funct3(funct3));
+  
+  main_decoder main_decoder (.ResultSrc(ResultSrc),
+                             .MemWrite(MemWrite),
+                             .ALUSrc(ALUSrc),
+                             .RegWrite(RegWrite),
+                             .ImmSrc(ImmSrc),
+                             .ALUOp(ALU),
+                             .Branch(Branch),
+                             .Op(Op));
+  
 endmodule
