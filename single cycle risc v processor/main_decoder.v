a2acc5a5c6840cdf9cd6bbbf974e446b650c518e
Daksh Hanswal <dakshhanswal@gmail.com>
2025-02-10 18:21:35 +0530

    Update main_decoder.v

--- single cycle risc v processor/main_decoder.v
+++ single cycle risc v processor/main_decoder.v
@@ -1,11 +1,11 @@
 //design
-module main_decoder (output PCSrc, ResultSrc, MemWrite, ALUSrc, RegWrite,
+module main_decoder (output ResultSrc, MemWrite, ALUSrc, RegWrite,
                      output [1:0] ImmSrc,
                      output [1:0] ALUOp,
-                     input Zero,
+                     output Branch,
                      input [6:0] Op);
   
-  wire Branch;
+  //wire Branch;
   
   assign RegWrite = ((Op == 7'b0000011) | (Op == 7'b0110011)) ? 1'b1 : 1'b0;
   assign ALUSrc = ((Op == 7'b0000011) | (Op == 7'b0100011)) ? 1'b1 : 1'b0;
@@ -15,7 +15,8 @@ module main_decoder (output PCSrc, ResultSrc, MemWrite, ALUSrc, RegWrite,
   assign ImmSrc = (Op == 7'b0100011) ? 2'b01 : (Op == 7'b1100011) ? 2'b10 : 2'b00;
   assign ALUOp = (Op == 7'b0110011) ? 2'b10 : (Op == 7'b1100011) ? 2'b01 : 2'b00;
   
-  assign PCSrc = Branch & Zero;
+  //assign PCSrc = Branch & Zero;
+  assign Branch = (Op == 7'b1100011) ? 1'b1 : 1'b0 ;
   
 endmodule
 
