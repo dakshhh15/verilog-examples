4ccb94b486e6f31af27066c6b0384cf3c077916a
Daksh Hanswal <dakshhanswal@gmail.com>
2025-02-10 14:50:07 +0530

    Create inst_mem.v

---
+++ single cycle risc v processor/inst_mem.v
@@ -0,0 +1,9 @@
+module Instrunction_Memory (output [31:0] RD,
+                            input [31:0] A,
+                            input rst);
+  
+  reg [31:0] Mem [1023:0];
+  
+  assign RD = (rst == 1'b0) ? 32'h00000000 : Mem[A[31:2]];
+  
+endmodule
