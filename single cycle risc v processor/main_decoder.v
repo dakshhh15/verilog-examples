79b3f28f3e5c2226ce75b3a326332b60d09c6a28
Daksh Hanswal <dakshhanswal@gmail.com>
2025-02-10 16:35:10 +0530

    Update main_decoder.v

--- single cycle risc v processor/main_decoder.v
+++ single cycle risc v processor/main_decoder.v
@@ -3,17 +3,17 @@ module main_decoder (output PCSrc, ResultSrc, MemWrite, ALUSrc, RegWrite,
                      output [1:0] ImmSrc,
                      output [1:0] ALUOp,
                      input Zero,
-                     input [6:0] op);
+                     input [6:0] Op);
   
   wire Branch;
   
-  assign RegWrite = ((op == 7'b0000011) | (op == 7'b0110011)) ? 1'b1 : 1'b0;
-  assign ALUSrc = ((op == 7'b0000011) | (op == 7'b0100011)) ? 1'b1 : 1'b0;
-  assign MemWrite = (op == 7'b0100011) ? 1'b1 : 1'b0;
-  assign ResultSrc = (op == 7'b0000011) ? 1'b1 : 1'b0;
-  assign Branch = (op == 7'b1100011) ? 1'b1 : 1'b0;
-  assign ImmSrc = (op == 7'b0100011) ? 2'b01 : (op == 7'b1100011) ? 2'b10 : 2'b00;
-  assign ALUOp = (op == 7'b0110011) ? 2'b10 : (op == 7'b1100011) ? 2'b01 : 2'b00;
+  assign RegWrite = ((Op == 7'b0000011) | (Op == 7'b0110011)) ? 1'b1 : 1'b0;
+  assign ALUSrc = ((Op == 7'b0000011) | (Op == 7'b0100011)) ? 1'b1 : 1'b0;
+  assign MemWrite = (Op == 7'b0100011) ? 1'b1 : 1'b0;
+  assign ResultSrc = (Op == 7'b0000011) ? 1'b1 : 1'b0;
+  assign Branch = (Op == 7'b1100011) ? 1'b1 : 1'b0;
+  assign ImmSrc = (Op == 7'b0100011) ? 2'b01 : (Op == 7'b1100011) ? 2'b10 : 2'b00;
+  assign ALUOp = (Op == 7'b0110011) ? 2'b10 : (Op == 7'b1100011) ? 2'b01 : 2'b00;
   
   assign PCSrc = Branch & Zero;
   
@@ -27,7 +27,7 @@ module tb;
   reg Zero;
   reg [6:0] op;
   
-  main_decoder i1 (.PCSrc(PCSrc), .ResultSrc(ResultSrc), .MemWrite(MemWrite), .ALUSrc(ALUSrc), .RegWrite(RegWrite), .ImmSrc(ImmSrc), .ALUOp(ALUOp), .Zero(Zero), .op(op));
+  main_decoder i1 (.PCSrc(PCSrc), .ResultSrc(ResultSrc), .MemWrite(MemWrite), .ALUSrc(ALUSrc), .RegWrite(RegWrite), .ImmSrc(ImmSrc), .ALUOp(ALUOp), .Zero(Zero), .Op(op));
   
   initial
     begin
