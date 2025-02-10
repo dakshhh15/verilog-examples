04192df95189647ef4a770181364b7d50859d547
Daksh Hanswal <dakshhanswal@gmail.com>
2025-02-10 14:53:12 +0530

    Create program_counter.v

---
+++ single cycle risc v processor/program_counter.v
@@ -0,0 +1,13 @@
+module Program_Counter (output reg [31:0] PC,
+                        input [31:0] PC_NEXR,
+                        input clk, rst);
+  
+  always @(posedge clk)
+    begin
+      if (rst == 0)
+        PC <= 0;
+      else 
+        PC <= PC_NEXT;
+    end
+  
+endmodule
