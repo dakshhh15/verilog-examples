6d6b66925858dc10bf344397b633514944f1ddcb
Daksh Hanswal <dakshhanswal@gmail.com>
2025-02-11 18:04:58 +0530

    Create single_cycle_top_tb.v

---
+++ single cycle risc v processor/single_cycle_top_tb.v
@@ -0,0 +1,33 @@
+//tested FFC4A303 inst which is to load a value. 
+
+module Single_Cycle_Top_Tb ();
+    
+    reg clk=1'b1,rst;
+
+    single_cycle_top Single_Cycle_Top(
+                                .clk(clk),
+                                .rst(rst)
+    );
+
+    initial begin
+        $dumpfile("Single Cycle.vcd");
+        $dumpvars(0);
+    end
+
+    always 
+    begin
+        clk = ~ clk;
+        #50;  
+        
+    end
+    
+    initial
+    begin
+        rst <= 1'b0;
+        #100;
+
+        rst <=1'b1;
+        #300;
+        $finish;
+    end
+endmodule
